adfcc3ba9ec9f931597af8fc949199bd6777681c
HaxSam <haxsam@pm.me>
2024-03-01 09:38:03 +0100

    fixing always updating rainbow mode

--- lib/drivers/rgb_backlight.h
+++ lib/drivers/rgb_backlight.h
@@ -98,12 +98,18 @@ uint8_t rgb_backlight_get_rainbow_saturation();
 void rgb_backlight_reconfigure(bool enabled);
 
 /**
+ * @brief Run the RGB rainbowmode
+ *
+ */
+void rgb_backlight_rainbow_mode();
+
+/**
  * @brief Apply current RGB lighting settings
  *
  * @param brightness Backlight intensity (0-255)
- * @param tick       Whether this update was a tick (for rainbow)
+ * @param forced force a update even brightness doesnt changed
  */
-void rgb_backlight_update(uint8_t brightness, bool tick);
+void rgb_backlight_update(uint8_t brightness, bool forced);
 
 #ifdef __cplusplus
 }
