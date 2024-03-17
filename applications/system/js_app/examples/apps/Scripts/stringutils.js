7f074176763ebeb995a0f122b9325babea77d6e4
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-03-17 06:19:46 +0000

    Move to js_thread + improve parse_int

--- applications/system/js_app/examples/apps/Scripts/stringutils.js
+++ applications/system/js_app/examples/apps/Scripts/stringutils.js
@@ -12,8 +12,8 @@ let searchStr = "World";
 let result2 = to_string(sampleText.indexOf(searchStr));
 print(result2);
 
-let upperCaseText = "Text in upper case: " + toUpperCase(sampleText);
+let upperCaseText = "Text in upper case: " + to_upper_case(sampleText);
 print(upperCaseText);
 
-let lowerCaseText = "Text in lower case: " + toLowerCase(sampleText);
+let lowerCaseText = "Text in lower case: " + to_lower_case(sampleText);
 print(lowerCaseText);
--- applications/system/js_app/js_thread.c
+++ applications/system/js_app/js_thread.c
@@ -8,7 +8,6 @@
 #include "js_thread.h"
 #include "js_thread_i.h"
 #include "js_modules.h"
-#include "modules/js_string.h"
 
 #define TAG "JS"
 
@@ -203,19 +202,29 @@ static void js_global_to_string(struct mjs* mjs) {
     mjs_return(mjs, ret);
 }
 
+static void js_global_to_hex_string(struct mjs* mjs) {
+    double num = mjs_get_int(mjs, mjs_arg(mjs, 0));
+    char tmp_str[] = "-FFFFFFFF";
+    itoa(num, tmp_str, 16);
+    mjs_val_t ret = mjs_mk_string(mjs, tmp_str, ~0, true);
+    mjs_return(mjs, ret);
+}
+
 static void js_parse_int(struct mjs* mjs) {
     mjs_val_t arg = mjs_arg(mjs, 0);
-    size_t str_len;
-    const char* str = NULL;
-    if(mjs_is_string(arg)) {
-        str = mjs_get_string(mjs, &arg, &str_len);
-    } else {
+    if(!mjs_is_string(arg)) {
+        mjs_return(mjs, mjs_mk_number(mjs, 0));
+        return;
+    }
+    size_t str_len = 0;
+    const char* str = mjs_get_string(mjs, &arg, &str_len);
+    if((str_len == 0) || (str == NULL)) {
         mjs_return(mjs, mjs_mk_number(mjs, 0));
         return;
     }
 
-    int num = 0;
-    int sign = 1;
+    int32_t num = 0;
+    int32_t sign = 1;
     size_t i = 0;
 
     if(str[0] == '-') {
@@ -237,12 +246,50 @@ static void js_parse_int(struct mjs* mjs) {
     mjs_return(mjs, mjs_mk_number(mjs, num));
 }
 
-static void js_global_to_hex_string(struct mjs* mjs) {
-    double num = mjs_get_int(mjs, mjs_arg(mjs, 0));
-    char tmp_str[] = "-FFFFFFFF";
-    itoa(num, tmp_str, 16);
-    mjs_val_t ret = mjs_mk_string(mjs, tmp_str, ~0, true);
-    mjs_return(mjs, ret);
+static void js_to_upper_case(struct mjs* mjs) {
+    mjs_val_t arg0 = mjs_arg(mjs, 0);
+
+    size_t str_len;
+    const char* str = NULL;
+    if(mjs_is_string(arg0)) {
+        str = mjs_get_string(mjs, &arg0, &str_len);
+    }
+    if(!str) {
+        mjs_return(mjs, MJS_UNDEFINED);
+        return;
+    }
+
+    char* upperStr = strdup(str);
+    for(size_t i = 0; i < str_len; i++) {
+        upperStr[i] = toupper(upperStr[i]);
+    }
+
+    mjs_val_t resultStr = mjs_mk_string(mjs, upperStr, ~0, true);
+    free(upperStr);
+    mjs_return(mjs, resultStr);
+}
+
+static void js_to_lower_case(struct mjs* mjs) {
+    mjs_val_t arg0 = mjs_arg(mjs, 0);
+
+    size_t str_len;
+    const char* str = NULL;
+    if(mjs_is_string(arg0)) {
+        str = mjs_get_string(mjs, &arg0, &str_len);
+    }
+    if(!str) {
+        mjs_return(mjs, MJS_UNDEFINED);
+        return;
+    }
+
+    char* lowerStr = strdup(str);
+    for(size_t i = 0; i < str_len; i++) {
+        lowerStr[i] = tolower(lowerStr[i]);
+    }
+
+    mjs_val_t resultStr = mjs_mk_string(mjs, lowerStr, ~0, true);
+    free(lowerStr);
+    mjs_return(mjs, resultStr);
 }
 
 #ifdef JS_DEBUG
@@ -272,16 +319,15 @@ static int32_t js_thread(void* arg) {
     struct mjs* mjs = mjs_create(worker);
     worker->modules = js_modules_create(mjs, worker->resolver);
     mjs_val_t global = mjs_get_global(mjs);
-
     mjs_set(mjs, global, "print", ~0, MJS_MK_FN(js_print));
     mjs_set(mjs, global, "delay", ~0, MJS_MK_FN(js_delay));
     mjs_set(mjs, global, "to_string", ~0, MJS_MK_FN(js_global_to_string));
     mjs_set(mjs, global, "to_hex_string", ~0, MJS_MK_FN(js_global_to_hex_string));
     mjs_set(mjs, global, "ffi_address", ~0, MJS_MK_FN(js_ffi_address));
     mjs_set(mjs, global, "require", ~0, MJS_MK_FN(js_require));
-    mjs_set(mjs, global, "parseint", ~0, MJS_MK_FN(js_parse_int));
-
-    string_utils_init(mjs);
+    mjs_set(mjs, global, "parse_int", ~0, MJS_MK_FN(js_parse_int));
+    mjs_set(mjs, global, "to_upper_case", ~0, MJS_MK_FN(js_to_upper_case));
+    mjs_set(mjs, global, "to_lower_case", ~0, MJS_MK_FN(js_to_lower_case));
 
     mjs_val_t console_obj = mjs_mk_object(mjs);
     mjs_set(mjs, console_obj, "log", ~0, MJS_MK_FN(js_console_log));
