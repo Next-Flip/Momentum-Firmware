a9fa9958bb308352f320204af54c46bbe48c193a
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-03-19 18:25:53 +0000

    Show bypass before extend

--- applications/main/momentum_app/scenes/momentum_app_scene_protocols.c
+++ applications/main/momentum_app/scenes/momentum_app_scene_protocols.c
@@ -2,9 +2,10 @@
 
 enum VarItemListIndex {
     VarItemListIndexSubghzFreqs,
-    VarItemListIndexSubghzExtend,
     VarItemListIndexSubghzBypass,
+    VarItemListIndexSubghzExtend,
     VarItemListIndexGpioPins,
+    VarItemListIndexFileNamingPrefix,
 };
 
 void momentum_app_scene_protocols_var_item_list_callback(void* context, uint32_t index) {
@@ -12,18 +13,18 @@ void momentum_app_scene_protocols_var_item_list_callback(void* context, uint32_t
     view_dispatcher_send_custom_event(app->view_dispatcher, index);
 }
 
-static void momentum_app_scene_protocols_subghz_extend_changed(VariableItem* item) {
-    MomentumApp* app = variable_item_get_context(item);
-    app->subghz_extend = variable_item_get_current_value_index(item);
-    variable_item_set_current_value_text(item, app->subghz_extend ? "ON" : "OFF");
-    app->save_subghz = true;
-    app->require_reboot = true;
-}
-
 static void xtreme_app_scene_protocols_subghz_bypass_changed(VariableItem* item) {
     MomentumApp* app = variable_item_get_context(item);
     app->subghz_bypass = variable_item_get_current_value_index(item);
     variable_item_set_current_value_text(item, app->subghz_bypass ? "ON" : "OFF");
+    app->save_subghz = true;
+    app->require_reboot = true;
+}
+
+static void momentum_app_scene_protocols_subghz_extend_changed(VariableItem* item) {
+    MomentumApp* app = variable_item_get_context(item);
+    app->subghz_extend = variable_item_get_current_value_index(item);
+    variable_item_set_current_value_text(item, app->subghz_extend ? "ON" : "OFF");
     app->save_subghz = true;
     app->require_reboot = true;
 }
@@ -45,14 +46,14 @@ void momentum_app_scene_protocols_on_enter(void* context) {
     variable_item_set_current_value_text(item, ">");
 
     item = variable_item_list_add(
-        var_item_list, "SubGHz Extend", 2, momentum_app_scene_protocols_subghz_extend_changed, app);
-    variable_item_set_current_value_index(item, app->subghz_extend);
-    variable_item_set_current_value_text(item, app->subghz_extend ? "ON" : "OFF");
-
-    item = variable_item_list_add(
         var_item_list, "SubGHz Bypass", 2, xtreme_app_scene_protocols_subghz_bypass_changed, app);
     variable_item_set_current_value_index(item, app->subghz_bypass);
     variable_item_set_current_value_text(item, app->subghz_bypass ? "ON" : "OFF");
+
+    item = variable_item_list_add(
+        var_item_list, "SubGHz Extend", 2, momentum_app_scene_protocols_subghz_extend_changed, app);
+    variable_item_set_current_value_index(item, app->subghz_extend);
+    variable_item_set_current_value_text(item, app->subghz_extend ? "ON" : "OFF");
 
     item = variable_item_list_add(var_item_list, "GPIO Pins", 0, NULL, app);
     variable_item_set_current_value_text(item, ">");
