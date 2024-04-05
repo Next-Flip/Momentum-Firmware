6aac86a3d8efa29300b6ca4f7f63f23a0f06de10
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-04-05 01:48:35 +0100

    Desktop: Fix early animation unload deadlocks

--- applications/services/desktop/scenes/desktop_scene_locked.c
+++ applications/services/desktop/scenes/desktop_scene_locked.c
@@ -86,7 +86,7 @@ bool desktop_scene_locked_on_event(void* context, SceneManagerEvent event) {
         switch(event.event) {
         case DesktopLockedEventOpenPowerOff: {
             if(momentum_settings.lockscreen_poweroff) {
-                loader_start(desktop->loader, "Power", "off", NULL);
+                loader_start_detached_with_gui_error(desktop->loader, "Power", "off");
             }
             consumed = true;
             break;
