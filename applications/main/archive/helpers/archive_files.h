30414e864be63217f91fe5570cbebc81677838a4
Zachary Weiss <zachary.weiss.acct@gmail.com>
2024-04-17 00:19:03 -0400

    Impl basic favorite ability for WAVs
    Namely just for the sake of favorite-ing the cart lock/unlock. Requires changes to WAV player ext fap, as to be PR'd from the Momentum-Apps repo.

--- applications/main/archive/helpers/archive_files.h
+++ applications/main/archive/helpers/archive_files.h
@@ -19,6 +19,7 @@ typedef enum {
     ArchiveFileTypeSubghzRemote,
     ArchiveFileTypeInfraredRemote,
     ArchiveFileTypeBadKb,
+    ArchiveFileTypeWAV,
     ArchiveFileTypeU2f,
     ArchiveFileTypeApplication,
     ArchiveFileTypeJS,
