190d286dbc39370e443dd61f54421bb752392fc1
zinongli <zl2242@nyu.edu>
2024-05-26 02:08:25 -0400

    Added verification through keys. Recognize new cards.

--- applications/main/nfc/plugins/supported_cards/csc.c
+++ applications/main/nfc/plugins/supported_cards/csc.c
@@ -7,49 +7,104 @@
 #include <nfc/nfc.h>
 #include <nfc/nfc_device.h>
 
-#define TAG "csc"
+#define TAG "CSC"
+#define csc_MAGIC_NUMBER 0x21
 
-#define csc_MAGIC_NUMBER 0x21  // Replace with your card's unique identifier
-//#define csc_VERSION 0x01       // Replace with your card's version if applicable
+typedef struct {
+    uint64_t a;
+    uint64_t b;
+} MfClassicKeyPair;
 
+static const MfClassicKeyPair csc_1k_keys[] = {
+    {.a = 0xF329F7AAEDFF, .b = 0xffffffffffff},
+    {.a = 0xF329F7AAEDFF, .b = 0xffffffffffff},
+    {.a = 0xF329F7AAEDFF, .b = 0xffffffffffff},
+    {.a = 0xF329F7AAEDFF, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xacffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+    {.a = 0xffffffffffff, .b = 0xffffffffffff},
+};
 
 static bool csc_verify(Nfc* nfc) {
-    //MfClassicData* data = mf_classic_alloc();
-    //furi_assert(nfc);
-    //furi_assert(device);
-    //nfc_device_copy_data(device, NfcProtocolMfClassic, data);
-
-    bool is_verified = false;
+    bool verified = false;
 
     do {
-        MfClassicType type = MfClassicType1k;
-        MfClassicError error = mf_classic_poller_sync_detect_type(nfc, &type);
-        if(error != MfClassicErrorNone || type != MfClassicType1k) {
-            FURI_LOG_D(TAG,"Card type detection failed or not Mifare Classic 1k\n");
+        const uint8_t verify_sector = 1;
+        const uint8_t verify_block = mf_classic_get_first_block_num_of_sector(verify_sector) + 1;
+        FURI_LOG_D(TAG, "Verifying sector %u", verify_sector);
+
+        MfClassicKey key = {0};
+        bit_lib_num_to_bytes_be(
+            csc_1k_keys[verify_sector].a, COUNT_OF(key.data), key.data);
+
+        MfClassicAuthContext auth_context;
+        MfClassicError error =
+            mf_classic_poller_sync_auth(nfc, verify_block, &key, MfClassicKeyTypeA, &auth_context);
+        if(error != MfClassicErrorNone) {
+            FURI_LOG_D(TAG, "Failed to read block %u: %d", verify_block, error);
             break;
         }
-        //data->type = type;
-        if(type != MfClassicType1k) break;
-
-        //block_data = data->block[0].data[0];
-        //printf("Block 0 Data: %02X %02X %02X %02X\n", block_data[0], block_data[1], block_data[2], block_data[3]);
-        //if(block_data[0] == MYCARD_MAGIC_NUMBER) {
-            is_verified = true;
-        //} else {
-        //    furi_string_printf("Verification failed: Magic number or version mismatch\n");
-        //}
-        FURI_LOG_D(TAG,"works as of verification");
+        verified = true;
     } while(false);
 
-    //mf_classic_free(data);
-    return is_verified;
+    return verified;
 }
 
-bool csc_read(Nfc* nfc, NfcDevice* device) {
-    // Custom read procedure for Mifare Classic 1k
+
+
+static bool csc_read(Nfc* nfc, NfcDevice* device) {
     furi_assert(nfc);
     furi_assert(device);
-    return true;
+
+    bool is_read = false;
+
+    MfClassicData* data = mf_classic_alloc();
+    nfc_device_copy_data(device, NfcProtocolMfClassic, data);
+
+    do {
+        MfClassicType type = MfClassicTypeMini;
+        MfClassicError error = mf_classic_poller_sync_detect_type(nfc, &type);
+        if(error != MfClassicErrorNone) break;
+
+        data->type = type;
+        if(type != MfClassicType1k) break;
+
+        MfClassicDeviceKeys keys = {
+            .key_a_mask = 0,
+            .key_b_mask = 0,
+        };
+        for(size_t i = 0; i < mf_classic_get_total_sectors_num(data->type); i++) {
+            bit_lib_num_to_bytes_be(
+                csc_1k_keys[i].a, sizeof(MfClassicKey), keys.key_a[i].data);
+            FURI_BIT_SET(keys.key_a_mask, i);
+            bit_lib_num_to_bytes_be(
+                csc_1k_keys[i].b, sizeof(MfClassicKey), keys.key_b[i].data);
+            FURI_BIT_SET(keys.key_b_mask, i);
+        }
+
+        error = mf_classic_poller_sync_read(nfc, &keys, data);
+        if(error == MfClassicErrorNotPresent) {
+            FURI_LOG_W(TAG, "Failed to read data");
+            break;
+        }
+
+        nfc_device_set_data(device, NfcProtocolMfClassic, data);
+
+        is_read = (error == MfClassicErrorNone);
+    } while(false);
+
+    mf_classic_free(data);
+
+    return is_read;
 }
 
 bool csc_parse(const NfcDevice* device, FuriString* parsed_data) {
@@ -57,43 +112,62 @@ bool csc_parse(const NfcDevice* device, FuriString* parsed_data) {
     bool parsed = false;
 
     do {
-        // Verify key
-        
         // Parse data
-        const uint8_t last_refill_block_num = 2;
+        const uint8_t refill_block_num = 2;
         const uint8_t current_balance_block_num = 4;
         const uint8_t card_lives_block_num = 9;
+        const uint8_t refill_sign_block_num = 13; 
+        const uint64_t new_card_sign = 0x00000000000000;
 
-        const uint8_t* last_refill_block_start_ptr =
-            &data->block[last_refill_block_num].data[9];
+        const uint8_t* refilled_balance_block_start_ptr =
+            &data->block[refill_block_num].data[9];
+        const uint8_t* refill_times_block_start_ptr =
+            &data->block[refill_block_num].data[5];
         const uint8_t* current_balance_block_start_ptr =
             &data->block[current_balance_block_num].data[0];
         const uint8_t* card_lives_block_start_ptr =
             &data->block[card_lives_block_num].data[0];
+        const uint8_t* refill_sign_block_start_ptr =
+            &data->block[refill_sign_block_num].data[0];
 
-        uint32_t last_refill_balance = bit_lib_bytes_to_num_le(last_refill_block_start_ptr, 2);
-        uint32_t last_refill_balance_lari = last_refill_balance / 100;
-        uint8_t last_refill_balance_tetri = last_refill_balance % 100;
+        uint32_t refilled_balance = bit_lib_bytes_to_num_le(refilled_balance_block_start_ptr, 2);
+        uint32_t refilled_balance_dollar = refilled_balance / 100;
+        uint8_t refilled_balance_cent = refilled_balance % 100;
 
         uint32_t current_balance = bit_lib_bytes_to_num_le(current_balance_block_start_ptr, 2);
-        uint32_t current_balance_lari = current_balance / 100;
-        uint8_t current_balance_tetri = current_balance % 100;
+        uint32_t current_balance_dollar = current_balance / 100;
+        uint8_t current_balance_cent = current_balance % 100;
 
         uint32_t card_lives = bit_lib_bytes_to_num_le(card_lives_block_start_ptr, 2);
+        uint32_t refill_times = bit_lib_bytes_to_num_le(refill_times_block_start_ptr, 2);
+
+        uint64_t refill_sign = bit_lib_bytes_to_num_le(refill_sign_block_start_ptr, 8);
 
         size_t uid_len = 0;
         const uint8_t* uid = mf_classic_get_uid(data, &uid_len);
-        uint32_t card_number = bit_lib_bytes_to_num_le(uid, 4);
+        uint32_t card_uid = bit_lib_bytes_to_num_le(uid, 4);
+
+        if(refill_sign == new_card_sign) {
+            furi_string_printf(
+            parsed_data,
+            "\e#CSC Service Works\nUID: %lu\nNew Card\nCard Price: %lu.%02u USD\nCard Usage Left: %lu",
+            card_uid,
+            refilled_balance_dollar,
+            refilled_balance_cent,
+            card_lives);
+        } else {
 
         furi_string_printf(
             parsed_data,
-            "\e#CSC Service Works\nCard number: %lu\nBalance: %lu.%02u USD\nLast Refill Balance: %lu.%02u USD\nCard Usage Left: %lu",
-            card_number,
-            current_balance_lari,
-            current_balance_tetri,
-            last_refill_balance_lari,
-            last_refill_balance_tetri,
+            "\e#CSC Service Works\nUID: %lu\nBalance: %lu.%02u USD\nRefilled Balance: %lu.%02u USD\nRefill Times: %lu\nCard Usage Left: %lu",
+            card_uid,
+            current_balance_dollar,
+            current_balance_cent,
+            refilled_balance_dollar,
+            refilled_balance_cent,
+            refill_times,
             card_lives);
+        }
         parsed = true;
     } while(false);
 
