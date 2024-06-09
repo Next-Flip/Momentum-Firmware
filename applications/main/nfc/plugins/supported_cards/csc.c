344aed50716d6699ab553f0d9927a74cde79733f
zinongli <131403964+zinongli@users.noreply.github.com>
2024-06-09 10:32:55 -0400

    Update csc.c

--- applications/main/nfc/plugins/supported_cards/csc.c
+++ applications/main/nfc/plugins/supported_cards/csc.c
@@ -41,22 +41,29 @@ bool csc_parse(const NfcDevice* device, FuriString* parsed_data) {
         const uint8_t refill_block_num = 2;
         const uint8_t current_balance_block_num = 4;
         const uint8_t current_balance_copy_block_num = 8;
-        const uint8_t* current_balance_block_start_ptr = &data->block[current_balance_block_num].data[0];
-        const uint8_t* current_balance_copy_block_start_ptr = &data->block[current_balance_copy_block_num].data[0];
-        uint32_t current_balance_and_times = bit_lib_bytes_to_num_le(current_balance_block_start_ptr, 4);
-        uint32_t current_balance_and_times_copy = bit_lib_bytes_to_num_le(current_balance_copy_block_start_ptr, 4);
-        const uint8_t* checksum_block = data->block[refill_block_num].data; //last byte of refill block is checksum
+        const uint8_t* current_balance_block_start_ptr =
+            &data->block[current_balance_block_num].data[0];
+        const uint8_t* current_balance_copy_block_start_ptr =
+            &data->block[current_balance_copy_block_num].data[0];
+        uint32_t current_balance_and_times =
+            bit_lib_bytes_to_num_le(current_balance_block_start_ptr, 4);
+        uint32_t current_balance_and_times_copy =
+            bit_lib_bytes_to_num_le(current_balance_copy_block_start_ptr, 4);
+        const uint8_t* checksum_block =
+            data->block[refill_block_num].data; //last byte of refill block is checksum
         uint8_t xor_result = 0;
-        for (size_t i = 0; i < 16; ++i) {
+        for(size_t i = 0; i < 16; ++i) {
             xor_result ^= checksum_block[i];
         } //perform the checksum
         if(current_balance_and_times != current_balance_and_times_copy) {
             FURI_LOG_D(TAG, "Backup verification failed");
-            break;} // failed verification if balance != backup
+            break;
+        } // failed verification if balance != backup
         if(current_balance_and_times == 0 || current_balance_and_times_copy == 0) {
             FURI_LOG_D(TAG, "Value bytes empty");
-            break;} // even if balance = 0, e.g. new card, refilled times can't be zero
-        // Parse data 
+            break;
+        } // even if balance = 0, e.g. new card, refilled times can't be zero
+        // Parse data
         const uint8_t card_lives_block_num = 9;
         const uint8_t refill_sign_block_num = 13;
         const uint8_t* refilled_balance_block_start_ptr = &data->block[refill_block_num].data[9];
@@ -70,15 +77,18 @@ bool csc_parse(const NfcDevice* device, FuriString* parsed_data) {
         uint32_t current_balance = bit_lib_bytes_to_num_le(current_balance_block_start_ptr, 2);
         uint32_t current_balance_dollar = current_balance / 100;
         uint8_t current_balance_cent = current_balance % 100;
-        uint32_t card_lives = bit_lib_bytes_to_num_le(card_lives_block_start_ptr, 2); // how many times it can still be used
-        uint32_t refill_times = bit_lib_bytes_to_num_le(refill_times_block_start_ptr, 2); 
-        uint64_t refill_sign = bit_lib_bytes_to_num_le(refill_sign_block_start_ptr, 8); 
+        uint32_t card_lives = bit_lib_bytes_to_num_le(
+            card_lives_block_start_ptr, 2); // how many times it can still be used
+        uint32_t refill_times = bit_lib_bytes_to_num_le(refill_times_block_start_ptr, 2);
+        uint64_t refill_sign = bit_lib_bytes_to_num_le(refill_sign_block_start_ptr, 8);
         // it is zero when you buy the card. but after refilling it, the refilling machine will leave a non-zero signature here
         size_t uid_len = 0;
         const uint8_t* uid = mf_classic_get_uid(data, &uid_len);
         uint32_t card_uid = bit_lib_bytes_to_num_le(uid, 4);
 
-        if(refill_sign == 0 && refill_times == 1) { // new cards don't comply to checksum but refill time should be once
+        if(refill_sign == 0 &&
+           refill_times ==
+               1) { // new cards don't comply to checksum but refill time should be once
             furi_string_printf(
                 parsed_data,
                 "\e#CSC Service Works\nUID: %lu\nNew Card\nCard Value: %lu.%02u USD\nCard Usages Left: %lu",
@@ -89,7 +99,8 @@ bool csc_parse(const NfcDevice* device, FuriString* parsed_data) {
         } else {
             if(xor_result != 0) {
                 FURI_LOG_D(TAG, "Checksum failed");
-            break; }
+                break;
+            }
             furi_string_printf(
                 parsed_data,
                 "\e#CSC Service Works\nUID: %lu\nBalance: %lu.%02u USD\nLast Top-up: %lu.%02u USD\nTop-up Count: %lu\nCard Usages Left: %lu",
