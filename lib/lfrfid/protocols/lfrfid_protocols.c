ead3610466da0eca3bfb1a5fe39552e376a9607e
Sean Skyhawk <166570980+skyhawkillusions@users.noreply.github.com>
2024-07-07 10:52:16 -0400

    Update lfrfid_protocols.c
    Add compatibility with protocol_insta_fob files

--- lib/lfrfid/protocols/lfrfid_protocols.c
+++ lib/lfrfid/protocols/lfrfid_protocols.c
@@ -4,6 +4,7 @@
 #include "protocol_h10301.h"
 #include "protocol_idteck.h"
 #include "protocol_indala26.h"
+#include "protocol_insta_fob.h"
 #include "protocol_io_prox_xsf.h"
 #include "protocol_awid.h"
 #include "protocol_fdx_a.h"
@@ -28,6 +29,7 @@ const ProtocolBase* lfrfid_protocols[] = {
     [LFRFIDProtocolH10301] = &protocol_h10301,
     [LFRFIDProtocolIdteck] = &protocol_idteck,
     [LFRFIDProtocolIndala26] = &protocol_indala26,
+    [LFRFIDProtocolInstaFob] = &protocol_insta_fob,
     [LFRFIDProtocolIOProxXSF] = &protocol_io_prox_xsf,
     [LFRFIDProtocolAwid] = &protocol_awid,
     [LFRFIDProtocolFDXA] = &protocol_fdx_a,
