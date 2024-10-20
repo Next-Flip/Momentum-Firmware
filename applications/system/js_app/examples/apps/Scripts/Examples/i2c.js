5df54cdd03424202e621158cba3f898194114c88
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-10-20 03:38:37 +0100

    toString() updates in example script

--- applications/system/js_app/examples/apps/Scripts/Examples/i2c.js
+++ applications/system/js_app/examples/apps/Scripts/Examples/i2c.js
@@ -18,7 +18,7 @@ if (addr === -1) {
     print("Please connect a 24C32N  EEPROM I2C device to the Flipper Zero.");
     print("SDA=pin 15, SCL=pin 16,  VCC=pin 9, GND=pin 8.");
 } else {
-    print("I2C device found at address: " + to_hex_string(addr));
+    print("I2C device found at address: " + addr.toString(16));
     delay(1000);
 
     // first two bytes are the start address (0x0000)
@@ -33,16 +33,16 @@ if (addr === -1) {
     // read 3 bytes - 0x42, 0x43, 0x44
     let data_buf = i2c.writeRead(addr, [0x00, 0x01], 3, 100);
     let data = Uint8Array(data_buf);
-    print("Read bytes: " + to_string(data.length));
+    print("Read bytes: " + data.length.toString());
     for (let i = 0; i < data.length; i++) {
-        print("data[" + to_string(i) + "] = " + to_hex_string(data[i]));
+        print("data[" + i.toString() + "] = " + data[i].toString(16));
     }
 
     // read two more bytes (0x45, 0x46) from current address
     data_buf = i2c.read(addr, 2);
     data = Uint8Array(data_buf);
-    print("Read bytes: " + to_string(data.length));
+    print("Read bytes: " + data.length.toString());
     for (let i = 0; i < data.length; i++) {
-        print("data[" + to_string(i) + "] = " + to_hex_string(data[i]));
+        print("data[" + i.toString() + "] = " + data[i].toString(16));
     }
 }
