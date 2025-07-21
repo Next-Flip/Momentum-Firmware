2c340b4264782e0ef3ee74e8b68aba43beb44965
WillyJL <me@willyjl.dev>
2025-07-22 01:35:32 +0200

    Keep ofw order here too

--- applications/services/dialogs/dialogs_message.h
+++ applications/services/dialogs/dialogs_message.h
@@ -10,8 +10,8 @@ extern "C" {
 typedef struct {
     const char* extension;
     bool skip_assets;
-    bool hide_dot_files;
     bool hide_ext;
+    bool hide_dot_files;
     const Icon* file_icon;
     FuriString* result_path;
     FuriString* preselected_filename;
