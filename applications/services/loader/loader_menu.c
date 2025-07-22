0bff69a545f0752f48901b4cb4d922f70b01cdcc
WillyJL <me@willyjl.dev>
2025-07-22 02:28:02 +0200

    Dont leave main menu when launching archive

--- applications/services/loader/loader_menu.c
+++ applications/services/loader/loader_menu.c
@@ -139,9 +139,6 @@ static void loader_menu_apps_callback(void* context, uint32_t index) {
         furi_record_close(RECORD_STORAGE);
 
         if(is_dir) {
-            furi_thread_flags_set(furi_thread_get_id(app->loader_menu->thread), 0);
-            if(app->loader_menu->closed_cb) app->loader_menu->closed_cb(app->loader_menu->context);
-
             Desktop* desktop = furi_record_open(RECORD_DESKTOP);
             desktop_launch_archive(desktop, menu_app->path);
             furi_record_close(RECORD_DESKTOP);
