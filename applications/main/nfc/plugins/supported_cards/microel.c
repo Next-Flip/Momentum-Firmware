2edba5f43576ce425bd78ad5ff6b8eafc0bd07eb
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2025-08-28 12:16:42 +0200

    Update microel.c

--- applications/main/nfc/plugins/supported_cards/microel.c
+++ applications/main/nfc/plugins/supported_cards/microel.c
@@ -7,7 +7,7 @@
 
 #define KEY_LENGTH 6
 #define UID_LENGTH 4
-// Offset per la conversione dell'anno (5 bit: da 0 a 31, quindi range 2010–2041)
+// offset for year conversion (5 bit: da 0 a 31 -> range 2010–2041)
 #define BASE_YEAR 2010
 
 typedef struct {
@@ -62,19 +62,17 @@ void generateKeyA(const uint8_t* uid, uint8_t uidSize, uint8_t keyA[]) {
 
     if(firstCharacter == 0x2 || firstCharacter == 0x3 || firstCharacter == 0xA ||
        firstCharacter == 0xB) {
-        // XOR con 0x40
         for(size_t i = 0; i < sizeof(sumHex); i++) {
             keyA[i] = 0x40 ^ sumHex[i];
         }
     } else if(
         firstCharacter == 0x6 || firstCharacter == 0x7 || firstCharacter == 0xE ||
         firstCharacter == 0xF) {
-        // XOR con 0xC0
         for(size_t i = 0; i < sizeof(sumHex); i++) {
             keyA[i] = 0xC0 ^ sumHex[i];
         }
     } else {
-        // KeyA uguale a sumHex
+        // key a same aa sumHex
         for(size_t i = 0; i < sizeof(sumHex); i++) {
             keyA[i] = sumHex[i];
         }
@@ -103,19 +101,18 @@ static bool microel_read(Nfc* nfc, NfcDevice* device) {
         MfClassicError error = mf_classic_poller_sync_detect_type(nfc, &type);
         if(error != MfClassicErrorNone) break;
 
-        //Get UID and check if it is 4 bytes
         size_t uid_len;
         const uint8_t* uid = mf_classic_get_uid(data, &uid_len);
         FURI_LOG_D(TAG, "UID identified: %02X%02X%02X%02X", uid[0], uid[1], uid[2], uid[3]);
         if(uid_len != UID_LENGTH) break;
 
-        // Generate keys
+        // generate keys
         uint8_t keyA[KEY_LENGTH];
         uint8_t keyB[KEY_LENGTH];
         generateKeyA(uid, UID_LENGTH, keyA);
         generateKeyB(keyA, KEY_LENGTH, keyB);
 
-        // Check key 0a to verify if it is a microel card
+        // check key 0a to verify if it is a microel card
         MfClassicKey key = {0};
         bit_lib_num_to_bytes_be(
             bit_lib_bytes_to_num_be(keyA, KEY_LENGTH), COUNT_OF(key.data), key.data);
@@ -127,7 +124,6 @@ static bool microel_read(Nfc* nfc, NfcDevice* device) {
             break;
         }
 
-        // Save keys generated to stucture
         for(size_t i = 0; i < mf_classic_get_total_sectors_num(data->type); i++) {
             if(microel_1k_keys[i].a == 0x000000000000) {
                 microel_1k_keys[i].a = bit_lib_bytes_to_num_be(keyA, KEY_LENGTH);
@@ -171,28 +167,23 @@ static bool microel_parse(const NfcDevice* device, FuriString* parsed_data) {
     bool parsed = false;
 
     do {
-        //Get UID
         size_t uid_len;
         const uint8_t* uid = mf_classic_get_uid(data, &uid_len);
         if(uid_len != UID_LENGTH) break;
 
-        // Generate key from uid
         uint8_t keyA[KEY_LENGTH];
         generateKeyA(uid, UID_LENGTH, keyA);
 
-        // Verify key
         MfClassicSectorTrailer* sec_tr =
             mf_classic_get_sector_trailer_by_sector(data, verify_sector);
         uint64_t key = bit_lib_bytes_to_num_be(sec_tr->key_a.data, 6);
         uint64_t key_for_check_from_array = bit_lib_bytes_to_num_be(keyA, KEY_LENGTH);
         if(key != key_for_check_from_array) break;
 
-        // Header info (UID, ATQA, SAK)
 	furi_string_cat_printf(parsed_data, "\e#Microel Card\n");
 	furi_string_cat_printf(parsed_data, "(Mifare Classic 1k)\n");
 	furi_string_cat_printf(parsed_data, "====================\n");
 
-	// UID
 	furi_string_cat_printf(parsed_data, "UID:");
 	for(size_t i = 0; i < UID_LENGTH; i++) {
     	    furi_string_cat_printf(parsed_data, " %02X", data->block[0].data[i]);
@@ -206,35 +197,26 @@ static bool microel_parse(const NfcDevice* device, FuriString* parsed_data) {
 	
 	furi_string_cat_printf(parsed_data, "--------------------\n");
 	
-	// Credito disponibile
         uint16_t credito_raw = (data->block[4].data[6] << 8) | data->block[4].data[5];
-	// Dati credito precedente (Block 5)
         uint16_t credito_precedente = (data->block[5].data[6] << 8) | data->block[5].data[5];
-	// Data e ora (Block 4, bytes 7-10) in formato MicroEL
 	uint32_t raw_data_op = (data->block[4].data[10] << 24) | (data->block[4].data[9] << 16) | (data->block[4].data[8] << 8) | data->block[4].data[7];
-	// Ricava tutti i campi (scarta i primi 5 bit)
-	uint32_t useful_bits = raw_data_op & 0x07FFFFFF; // ultimi 27 bit
+	uint32_t useful_bits = raw_data_op & 0x07FFFFFF;
 
 	uint8_t minuti     = (useful_bits >> 21) & 0x3F; // 6 bit
 	uint8_t ore        = (useful_bits >> 16) & 0x1F; // 5 bit
 	uint8_t tipo_op    = (useful_bits >> 14) & 0x03; // 2 bit
 	uint16_t anno      = ((useful_bits >> 9) & 0x1F) + BASE_YEAR; // 5 bit + offset
 	uint8_t mese       = (useful_bits >> 5) & 0x0F;  // 4 bit
-	uint8_t giorno     = useful_bits & 0x1F;         // ultimi 5 bit
+	uint8_t giorno     = useful_bits & 0x1F;         // lasts 5 bit
 
-	// Conversione tipo operazione in stringa
 	const char* tipo_op_str;
 	if(tipo_op == 0) tipo_op_str = "Firts operation"; // 00
 	else if(tipo_op == 1) tipo_op_str = "Recharge";   // 01
 	else tipo_op_str = "Payment";  // 10
 
-        // Numero operazione (Block 4, bytes 0-1)
         uint16_t num_op = (data->block[4].data[1] << 8) | data->block[4].data[0];
-        // Saldo punti (Block 4, bytes 11-12)
         uint16_t saldo_punti = (data->block[4].data[11] << 8) | data->block[4].data[12];
-        // Ultima transazione (Block 4, bytes 13-14)
         uint16_t transazione_raw = (data->block[4].data[14] << 8) | data->block[4].data[13];
-        // Somma in ingresso e cauzione (Block 4)
 	uint16_t somma_raw = data->block[4].data[2] | (data->block[4].data[3] << 8);
 	float somma = somma_raw / 10.0f;
 	bool cauzione = data->block[4].data[4] != 0;
@@ -249,15 +231,12 @@ static bool microel_parse(const NfcDevice* device, FuriString* parsed_data) {
         furi_string_cat_printf(parsed_data, "Op. Type: [%s]\n", tipo_op_str);       
         furi_string_cat_printf(parsed_data, "====================\n");
 
-	// Vendor ID - Block 1 (entire block)
         furi_string_cat_printf(parsed_data, "Vendor Univocal ID:\n");
 	for (size_t i = 0; i < 16; i++) {
     	    furi_string_cat_printf(parsed_data, "%02X", data->block[1].data[i]);
     	    if (i % 8 == 7) {
-                // Dopo ogni 6 byte, vai a capo
                 furi_string_cat_printf(parsed_data, "\n");
     	    } else {
-        	// Altrimenti, aggiungi uno spazio
         	furi_string_cat_printf(parsed_data, " ");
     	    }
         }
@@ -267,7 +246,6 @@ static bool microel_parse(const NfcDevice* device, FuriString* parsed_data) {
         furi_string_cat_printf(parsed_data, "Points balance: (%d pt.)\n", saldo_punti);
 	furi_string_cat_printf(parsed_data, "Admission credit: %d.%02d\n", (int)somma, (int)(somma * 100) % 100);
 	
-	// Gestione deposito aggiornata
 	if (cauzione) {
 	    furi_string_cat_printf(parsed_data, "Deposit: [Yes] ~ (%d.%02d)\n", 
 	                          cauzione_raw / 100, cauzione_raw % 100);
